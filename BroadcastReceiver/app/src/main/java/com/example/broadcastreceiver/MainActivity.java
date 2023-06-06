b5d2be3a8d0e28bb8fed54eb955679cae04efd87
balakrishna.g <balakrishna.s@marketsimplified.com>
2023-06-06 18:02:19 +0530

    Used Broadcast Receiver

---
+++ BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.broadcastreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        IntentFilter intentFilter= new IntentFilter("android.intent.action.BATTERY_LOW");
+        MyReceiver objReceiver = new MyReceiver();
+        registerReceiver(objReceiver,intentFilter);
+
+    }
+//    public void broadcastIntent(View view){
+//        Intent intent = new Intent();
+//        intent.setAction("android.intent.action.BATTERY_LOW");
+//        intent.setFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+//        sendBroadcast(intent);
+//    }
+}
